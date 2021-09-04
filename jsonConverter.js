06d57d86d183da6390d5b3913dab4502d8850297
serogbp <49165676+serogbp@users.noreply.github.com>
2021-09-04 17:30:29 +0200

    Falla obtener array de parents

--- jsonConverter.js
+++ jsonConverter.js
@@ -1,97 +1,257 @@
+// alpine.js x-data for both textareas
 const userInput = () => {
 	return {
 		input: '',
 		convertedInput: '',
 
 		convertInput() {
-			this.convertedInput = convertToJson(this.input);
+			this.convertedInput = text2Json(this.input);
 		},
 
 	}
 }
 
-function convertToJson(userInput) {
-	var myJson = {};
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+// Process user input
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+function text2Json(userInput) {
+	var json = {};
 
-	// Read userInput line by line getting the indentation (number of spaces to the left)
-	var lines = inputWithDepth(userInput);
+	userInput = getInfoOfEachLine(userInput);
 
-	for (var i = 0; i < lines.length; i++) {
-		let currentLine = lines[i];
+	for (var i = 0; i < userInput.length; i++) {
+		let currentLine = userInput[i];
 
-		let parentsChain = [];
-		parentsChain = getParentObjectChain(lines, i, []);
+		let parentArray = getParentArrayOfLine(userInput, i, []);
+		parentArray.pop(); // Remove last item, that is the currentLine
 
-		let a = {};
-		// let b = parentsChain.shift()
-		// a[b.object.property[0]] = b.object.value;
+		let parentType;
+		let parentChain;
 
-		let parents;
-		if (parentsChain.length > 1) {
-			parents = getParentObject(parentsChain, a);
+		if (parentArray.length > 0) {
+			parentType = parentArray[parentArray.length - 1].type;
+			parentChain = getParentChainOfParentArray(parentArray, {});
 		}
 
+		if (parentChain != null) {
+			switch (parentType) {
 
-		if (parents !== currentLine && 	parents != null) {
-			switch (parents[parents.length - 2].type) { // Type of the direct parent
-				case TYPE.PROPERTY: {
-					// myJson[parent.object.property][currentLine.object.property] = currentLine.object.value;
-					insertObjectToTarget(myJson[parents.object.property], currentLine);
-					break;
-				}
-				case TYPE.ARRAY: {
-					insertObjectToTarget(myJson[parents.object.property], currentLine);
-					break;
-				}
-				case TYPE.OBJECT: {
-					insertObjectToTarget(myJson[parents.object.property], currentLine);
-					break;
-				}
-				case TYPE.SIMPLE_TYPE: {
-					break;
-				}
 			}
 		} else {
-			insertObjectToTarget(myJson, currentLine);
+			appendLineToTarget(currentLine, json);
 		}
 	}
-	return JSON.stringify(myJson, undefined, 4);
-}
-
-function convertToText(userInput) {
-
+	return JSON.stringify(json, undefined, 4);
 }
 
 // Read user input line by line
 // And return an Array with:
-// - The line as String
 // - Identation level (number of spaces to the left)
-function inputWithDepth(userInput) {
+// - Object with the information the user input
+function getInfoOfEachLine(userInput) {
 	let array = [];
 	let lines = userInput.split('\n');
+
 	for (var i = 0; i < lines.length; i++) {
-		let depth = countPrefixSpaces(lines[i])
-		let type = getType(lines[i]);
-		let object = getJsonObject(lines[i], type);
-		array.push({
-			"line": lines[i],
-			"depth": depth,
-			"type": type,
-			"object": object,
-		});
+		let line = lines[i];
+		let type = getType(line);
+
+		// If is not comment, push line to array
+		if (type != -1) {
+			let indentation = line.countStartSpaces();
+			let object;
+
+			switch (type) {
+				case TYPE.PROPERTY: {
+					object = new JsonProperty(line);
+					break;
+				}
+				case TYPE.ARRAY: {
+					object = new JsonArray(line);
+					break;
+				}
+				case TYPE.OBJECT: {
+					object = new JsonObject(line);
+					break;
+				}
+				case TYPE.PRIMITIVE: {
+					object = new JsonPrimitive(line);
+					break;
+				}
+			}
+
+			array.push(new JsonLine({
+				"indentation": indentation,
+				"type": type,
+				"object": object,
+			}));
+		}
 	}
+	console.log(array);
 	return array;
 }
 
-// var parent
-// for parents
-// parent = parents[i]
+function getParentArrayOfLine(lines, currentLineIndex, array) {
+	let currentLine = lines[currentLineIndex];
+	let parent;
+
+	for (var i = currentLineIndex - 1; i >= 0; i--) {
+		if (lines[i].indentation < currentLine.indentation) {
+			array = getParentArrayOfLine(lines, i, array);
+		}
+	}
+
+	// array.push((parent != null) ? parent: currentLine);
+
+	let indentation = (parent != null) ? parent.indentation: currentLine.indentation;
+	let type = (parent != null) ? parent.type: currentLine.type;
+	let object = (parent != null) ? parent.object: currentLine.object;
+
+	array.push( new JsonLine({
+		"indentation": indentation,
+		"type": type,
+		"object": object,
+	}));
+
+	return array;
+}
+
+function getParentChainOfParentArray(parentArray, parentChain) {
+	let lastParent;
+	while (parentArray.length > 0) {
+		let currentParent = parentArray.shift();
+
+		if (lastParent != null) {
+			switch (lastParent.type) {
+				case TYPE.PROPERTY:
+					parentChain[lastParent.object.name][currentParent.object.name] = [currentParent.object.value];
+					break;
+				case TYPE.ARRAY:
+					parentChain[lastParent.object.name][currentParent.object.name] = [];
+					break;
+				case TYPE.OBJECT:
+					parentChain[lastParent.object.name][currentParent.object.name] = {};
+					break;
+				case TYPE.PRIMITIVE:
+					parentChain[lastParent.object.name].push(currentParent.object.name);
+					break;
+			}
+		} else { // First parent
+			switch (currentParent.type) {
+				case TYPE.PROPERTY:
+					parentChain[currentParent.object.name] = [currentParent.object.value];
+					break;
+				case TYPE.ARRAY:
+					parentChain[currentParent.object.name] = [];
+					break;
+				case TYPE.OBJECT:
+					parentChain[currentParent.object.name] = {};
+					break;
+				case TYPE.PRIMITIVE:
+					// TODO ???
+					break;
+			}
+		}
+		lastParent = currentParent;
+	}
+	return parentChain;
+}
+
+function appendLineToTarget(line, target) {
+	if (line instanceof JsonLine) {
+		switch (line.type) {
+			case TYPE.PROPERTY: {
+				target[line.object.name] = line.object.value;
+				break;
+			}
+			case TYPE.ARRAY: {
+				target[line.object.name] = [];
+				break;
+			}
+			case TYPE.OBJECT: {
+				target[line.object.name] = {};
+				break;
+			}
+			case TYPE.PRIMITIVE: {
+				target.push(line.object.name);
+			}
+		}
+	}
+}
+
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+// Classes
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+class JsonLine {
+	constructor(line) {
+		this.indentation = line.indentation;
+		this.type = line.type;
+		this.object = line.object;
+	}
+}
+
+class JsonProperty {
+	constructor(input) {
+		this.name = this._getName(input);
+		this.value = this._getValue(input);
+	}
+
+	// e.g. "property = value" => "property"
+	_getName(input) {
+		return regexPropertyName.exec(input)[1].trim();
+	}
+
+	// e.g. "property = value" => "value"
+	// 		"property = 5.5" => 5.5
+	//		"property = True" => true
+	_getValue(input) {
+		input = regexPropertyValue.exec(input)[1].trim();
+
+		if (!isNaN(parseFloat(input))) return parseFloat(input);
+		else if (input.toLowerCase() === 'true') return true;
+		else if (input.toLowerCase() === 'false') return false;
+		else return input;
+	}
+}
+
+class JsonArray {
+	constructor(input) {
+		this.name = this._getName(input);
+	}
+
+	_getName(input) {
+		return regexArray.exec(input)[1].trim();
+	}
+}
+
+class JsonObject {
+	constructor(input) {
+		this.name = this._getName(input);
+	}
+
+	_getName(input) {
+		return regexObject.exec(input)[1].trim();
+	}
+}
+
+class JsonPrimitive {
+	constructor(input) {
+		this.name = this._getName(input);
+	}
+
+	_getName(input) {
+		return regexPrimitive.exec(input)[1].trim();
+	}
+}
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+// Extend methods
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 // Returns de number of spaces a string starts with
-function countPrefixSpaces(s) {
+String.prototype.countStartSpaces = function () {
 	var count = 0
-	for (var i = 0; i < s.length; i++) {
-		if (s[i].trim() === '') {
+	for (var i = 0; i < this.length; i++) {
+		if (this[i].trim() === '') {
 			count++;
 		} else {
 			break;
@@ -100,77 +260,15 @@ function countPrefixSpaces(s) {
 	return count;
 }
 
-function getJsonObject(line, type) {
-	let jsonObject = {
-		"property": "",
-		"value": "",
-	}
-	switch (type) {
-		case TYPE.PROPERTY: {
-			let property = getProperty(line);
-			let value = getValue(line);
-			if (property !== null && value !== null) {
-				jsonObject.property = property;
-				jsonObject.value = value;
-			}
-			break;
-		}
-		case TYPE.ARRAY: {
-			let property = getProperty(line);
-			jsonObject.property = [property]
-			break;
-		}
-		case TYPE.OBJECT: {
-			let property = getPropertyObject(line);
-			jsonObject.property = [property]
-			break;
-		}
-		case TYPE.SIMPLE_TYPE: {
-			let property = getProperty(line);
-			jsonObject.property = [property]
-			break;
-		}
-	}
-
-	return jsonObject;
-}
-
-function insertObjectToTarget(target, currentLine) {
-	switch (currentLine.type) {
-		case TYPE.PROPERTY: {
-			target[currentLine.object.property] = currentLine.object.value;
-			break;
-		}
-		case TYPE.ARRAY: {
-			target[currentLine.object.property] = [];
-			break;
-		}
-		case TYPE.OBJECT: {
-			target[currentLine.object.property] = {};
-			break;
-		}
-		case TYPE.SIMPLE_TYPE: {
-			target.push(currentLine.object.property);
-			break;
-		}
-	}
-}
-
-// e.g. "property = value" => "property"
-// e.g. "array[]" => "array"
-const getProperty = (userInput) => /(.*)(=|\[])/.exec(userInput)[1].trim();
-// For TYPE.OBJECT
-const getPropertyObject = (userInput) => /(.*)/.exec(userInput)[1].trim();
-
-// e.g. "property = value" => "value"
-const getValue = (userInput) => /=(.*)/.exec(userInput)[1].trim();
-
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+// Code for getting the type of data the user inputs
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 const TYPE = {
 	COMMENT: -1,
 	PROPERTY: 0,
 	ARRAY: 1,
 	OBJECT: 2,
-	SIMPLE_TYPE: 3,
+	PRIMITIVE: 3,
 }
 
 function getType(userInput) {
@@ -179,59 +277,25 @@ function getType(userInput) {
 	else if (isProperty(userInput)) type = TYPE.PROPERTY
 	else if (isArray(userInput)) type = TYPE.ARRAY
 	else if (isObject(userInput)) type = TYPE.OBJECT
-	else if (isSimpleType(userInput)) type = TYPE.SIMPLE_TYPE
+	else if (isPrimitive(userInput)) type = TYPE.PRIMITIVE
 	return type;
 }
 
-// e.g. "// This is a comment" => true
-const isComment = (userInput) => /^\/.*$/.test(userInput);
+const isComment = (userInput) => regexComment.test(userInput);
 
-// e.g. "property = value" => true
-const isProperty = (userInput) => /\S+\s*=\s*\S+/.test(userInput);
+const isProperty = (userInput) => regexProperty.test(userInput);
 
-// e.g. "someText[]" => true
-const isArray = (userInput) => /\S\[]/.test(userInput)
+const isArray = (userInput) => regexArray.test(userInput)
 
-// e.g. "objectName" => true
-const isObject = (userInput) => /\S+/.test(userInput);
+const isObject = (userInput) => regexObject.test(userInput);
 
-// e.g. "arrayItem," => true
-const isSimpleType = (userInput) => /\S+,/.test(userInput);
+const isPrimitive = (userInput) => regexPrimitive.test(userInput);
 
+const regexComment = /^\/.*$/;			// e.g. "// This is a comment" => true
+const regexProperty = /\S+\s*=\s*\S+/;	// e.g. "property = value" => true
+const regexArray = /(\S*)\[]/;			// e.g. "someText[]" => true
+const regexObject = /(\S*){}/;			// e.g. "objectName" => true
+const regexPrimitive = /(\S+),/;		// e.g. "arrayItem," => true
 
-
-
-
-
-function getParentObjectChain(list, index, array) {
-	let depth = list[index].depth;
-	let parent;
-
-	let children = list[index];
-
-	for (var i = index - 1; i >= 0; i--) {
-		if (list[i].depth < depth) array = getParentObjectChain(list, i, array);
-	}
-	if (parent!=null){
-		array.push(parent);
-	} else {
-		array.push(children);
-	}
-	return array;
-}
-
-function getParentObject(array, parent) {
-	let lastObjectName = "";
-	while (array.length > 0) {
-		let object = array.shift();
-		let objectName = object.object.property[0];
-		if (lastObjectName != "") {
-			parent[lastObjectName][objectName] = {};
-		} else {
-			parent[objectName] = {};
-		}
-		lastObjectName = objectName;
-	}
-
-	return parent;
-}
+const regexPropertyName = /(.*)(=)/;	// e.g. "property = value" => "property"
+const regexPropertyValue = /=(.*)/;		// e.g. "property = value" => "value"
